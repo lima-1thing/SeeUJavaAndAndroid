af946e96c6bbc098c98295066cbca8b9ae0095a4
Li Ma <lima@infolexllc.com>
2021-05-15 14:00:01 -0400

    Initial commit.

---
+++ app/calculator/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -0,0 +1,26 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+    public void onClick0(View view) {
+        TextView txtView = this.findViewById(R.id.textView);
+        txtView.setText("0");
+    }
+
+    public void onClick1(View view) {
+        TextView txtView = this.findViewById(R.id.textView);
+        txtView.setText("1");
+    }
+}
