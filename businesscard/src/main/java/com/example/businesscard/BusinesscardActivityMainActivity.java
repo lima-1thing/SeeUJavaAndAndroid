42edaababf90874669d965e524a01b03a0eac973
Li Ma <lima@infolexllc.com>
2021-05-16 12:42:55 -0400

    Add business card

---
+++ businesscard/src/main/java/com/example/businesscard/BusinesscardActivityMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.businesscard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusinesscardActivityMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.businesscard_activity_main);
+    }
+}
