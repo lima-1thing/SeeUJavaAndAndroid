e6156021d030488bf84a70f04bea99c68b908ef6
Li Ma <lima01@gmail.com>
2021-06-26 10:42:27 -0400

    Fix bug. Need to implement hashCode and equals in Merchandise to make it work properly as a Map key.

--- shoppingcart2/src/main/java/com/example/multiscreenapp/items/Merchandise.java
+++ shoppingcart2/src/main/java/com/example/multiscreenapp/items/Merchandise.java
@@ -1,5 +1,7 @@
 package com.example.multiscreenapp.items;
 
+import androidx.annotation.Nullable;
+
 import java.io.Serializable;
 
 public class Merchandise implements Serializable {
@@ -11,6 +13,21 @@ public class Merchandise implements Serializable {
         this.name = name;
         this.imageId = imageId;
         this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        return String.format("%d|%s|%.2f", this.imageId, this.name, this.price)
+                .hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if ( null == obj ) return false;
+        Merchandise item = (Merchandise) obj;
+        return this.name.equals(item.name)
+                && this.imageId == item.imageId
+                && Math.abs(this.price - item.price) < 0.0001;
     }
 
     public String getName() {
